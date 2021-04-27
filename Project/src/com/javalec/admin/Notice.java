ac5215d0463c8e9470cc387df5708afa39389763
Yang seolin <81757940+bisor0627@users.noreply.github.com>
2021-04-27 14:44:21 +0900

    0427
    2:44

---
+++ Project/src/com/javalec/admin/Notice.java
@@ -0,0 +1,78 @@
+package com.javalec.admin;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
+
+public class Notice {
+	
+	private final DefaultTableModel Outer_Table = new DefaultTableModel();
+	private JFrame frame;
+	private JButton btnNewNotice;
+	private JScrollPane scrollPane;
+	private JTable Inner_Table;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Notice window = new Notice();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Notice() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().add(getBtnNewNotice());
+		frame.getContentPane().add(getScrollPane());
+	}
+	private JButton getBtnNewNotice() {
+		if (btnNewNotice == null) {
+			btnNewNotice = new JButton("작성하기");
+			btnNewNotice.setBounds(337, 6, 96, 29);
+		}
+		return btnNewNotice;
+	}
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setBounds(32, 60, 400, 182);
+			scrollPane.setViewportView(getInner_Table());
+		}
+		return scrollPane;
+	}
+	private JTable getInner_Table() {
+		if (Inner_Table == null) {
+			Inner_Table = new JTable();
+			Inner_Table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			Inner_Table.setModel(Outer_Table);
+		}
+		return Inner_Table;
+	}
+	//이너테이블과 아우터테이블 연결까ㅣㅈ 완료, 데이터 가져오는 쿼리문 작성 안함
+}
--- Project/src/com/javalec/frame/ClothesFrame.java
+++ Project/src/com/javalec/frame/ClothesFrame.java
@@ -3,24 +3,19 @@ package com.javalec.frame;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JLayeredPane;
-import javax.swing.JDesktopPane;
-import javax.swing.JSplitPane;
 import javax.swing.JLabel;
+import javax.swing.Icon;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
-import javax.swing.JTextField;
 
-public class PostFrame {
+public class ClothesFrame {
 
 	private JFrame frame;
 	private JLabel ClothesData;
-	private JLabel userName;
-	private JLabel PostTitle;
-	private JLabel userImage;
+	private JLabel ClothesName;
 	private JLabel likeImage;
 
 	/**
@@ -30,7 +25,7 @@ public class PostFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PostFrame window = new PostFrame();
+					ClothesFrame window = new ClothesFrame();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -42,7 +37,7 @@ public class PostFrame {
 	/**
 	 * Create the application.
 	 */
-	public PostFrame() {
+	public ClothesFrame() {
 		initialize();
 	}
 
@@ -55,11 +50,10 @@ public class PostFrame {
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().add(getClothesData());
-		frame.getContentPane().add(getUserName());
-		frame.getContentPane().add(getPostTitle());
-		frame.getContentPane().add(getUserImage());
+		frame.getContentPane().add(getClothesName());
 		frame.getContentPane().add(getLikeImage());
 	}
+
 	private JLabel getClothesData() {
 		if (ClothesData == null) {
 			ImageIcon icon = new ImageIcon("/Users/biso/Downloads/clothes.png");
@@ -72,33 +66,13 @@ public class PostFrame {
 		}
 		return ClothesData;
 	}
-	private JLabel getUserName() {
-		if (userName == null) {
-			userName = new JLabel("name");
-			userName.setBounds(42, 215, 61, 16);
-			userName.setFont(new Font("Lucida Grande", Font.PLAIN, 11));
+	private JLabel getClothesName() {
+		if (ClothesName == null) {
+			ClothesName = new JLabel("Title");
+			ClothesName.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+			ClothesName.setBounds(24, 215, 61, 16);
 		}
-		return userName;
-	}
-	private JLabel getPostTitle() {
-		if (PostTitle == null) {
-			PostTitle = new JLabel("Title");
-			PostTitle.setBounds(42, 237, 61, 16);
-			PostTitle.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
-		}
-		return PostTitle;
-	}
-	private JLabel getUserImage() {
-		if (userImage == null) {
-			ImageIcon icon = new ImageIcon("/Users/biso/Downloads/user.png");
-			Image img = icon.getImage();
-			Image changeImage = img.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
-			ImageIcon changeIcon = new ImageIcon(changeImage);
-			userImage = new JLabel(changeIcon);
-			userImage.setBounds(6, 219, 35, 34);
-			userImage.setHorizontalAlignment(SwingConstants.CENTER);
-		}
-		return userImage;
+		return ClothesName;
 	}
 	private JLabel getLikeImage() {
 		if (likeImage == null) {
@@ -107,7 +81,7 @@ public class PostFrame {
 			Image changeImage = img.getScaledInstance(28, 28, Image.SCALE_SMOOTH);
 			ImageIcon changeIcon = new ImageIcon(changeImage);
 			likeImage = new JLabel(changeIcon);
-			likeImage.setBounds(167, 224, 30, 29);
+			likeImage.setBounds(159, 209, 30, 29);
 			likeImage.setHorizontalAlignment(SwingConstants.CENTER);
 		}
 		return likeImage;
